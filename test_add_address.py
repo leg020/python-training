ec40cfb836078b65008df37e81e6e3d2afb8d2b7
Александр <leg020@inbox.ru>
2019-04-25 23:47:45 -0700

    Разбил на методы address

--- test_add_address.py
+++ test_add_address.py
@@ -6,86 +6,91 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
 
-class UntitledTestCase(unittest.TestCase):
+class NewAddress(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_untitled_test_case(self):
+    def test_new_address(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/edit.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_address(wd)
+        self.return_home(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_home(self, wd):
+        wd.find_element_by_link_text("home").click()
+
+    def create_address(self, wd):
+        # open page
         wd.find_element_by_link_text("add new").click()
+        # fill address
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("defcr")
+        wd.find_element_by_name("firstname").send_keys("dsf")
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("eced")
+        wd.find_element_by_name("middlename").send_keys("dcsc")
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("rfr")
+        wd.find_element_by_name("lastname").send_keys("cdsc")
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("rfrefe")
+        wd.find_element_by_name("nickname").send_keys("sdcs")
         #wd.find_element_by_name("photo").click()
         #wd.find_element_by_name("photo").clear()
-        #wd.find_element_by_name("photo").send_keys("C:\\fakepath\\header.jpg")
-        #wd.find_element_by_name("title").click()
-        #wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("dcd")
+        #wd.find_element_by_name("photo").send_keys("C:\\fakepath\\title[1].gif")
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys("rdewd")
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("cdsc")
+        wd.find_element_by_name("company").send_keys("sdassdasa")
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("dcdcs")
+        wd.find_element_by_name("address").send_keys("cdss")
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("cdsc")
+        wd.find_element_by_name("home").send_keys("scac")
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("dcdscdsc")
+        wd.find_element_by_name("mobile").send_keys("sacas")
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("dcd")
+        wd.find_element_by_name("work").send_keys("asxsax")
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("dcd")
+        wd.find_element_by_name("fax").send_keys("sxsa")
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("vfvf")
+        wd.find_element_by_name("email").send_keys("sxcsa")
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("vffv")
+        wd.find_element_by_name("email2").send_keys("sac")
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("dvdd")
+        wd.find_element_by_name("email3").send_keys("sc")
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("cddc")
+        wd.find_element_by_name("homepage").send_keys("sc")
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("10")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text("15")
         wd.find_element_by_name("bday").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("June")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("September")
         wd.find_element_by_name("bmonth").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1994")
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("14")
+        Select(wd.find_element_by_name("aday")).select_by_visible_text("12")
         wd.find_element_by_name("aday").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("October")
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text("May")
         wd.find_element_by_name("amonth").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
@@ -94,16 +99,29 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_name("new_group").click()
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("swws")
+        wd.find_element_by_name("address2").send_keys("xxxx")
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("cfcfcfc")
+        wd.find_element_by_name("phone2").send_keys("xsw")
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("dedede")
+        wd.find_element_by_name("notes").send_keys("wece")
+        # submit address
         wd.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
-        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
@@ -118,8 +136,6 @@ class UntitledTestCase(unittest.TestCase):
         except NoAlertPresentException as e:
             return False
         return True
-
-
 
     def tearDown(self):
         self.wd.quit()
