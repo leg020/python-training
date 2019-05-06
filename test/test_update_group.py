9da0771f24fd70764b093f94d8963b1d8b48ecc0
Александр <leg020@inbox.ru>
2019-05-05 17:49:32 -0700

    Оптимизация ввода данных: добавлены функции обновления одного поля ввода и проверка

--- test/test_update_group.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Alex'
-from model.group import Group
-
-def test_update_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.update_group(Group(name="111", header="222", footer="333"))
-    app.session.logout()
