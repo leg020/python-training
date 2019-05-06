7edad2b1295e5056b061214f22439a6fcd7d41ea
Александр <leg020@inbox.ru>
2019-05-06 00:11:57 -0700

    Создание интелектуальной фикстуры

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,11 +3,7 @@ from model.group import Group
 
 
 def test_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="ddc", header="ddc", footer="dcdc"))
-    app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
--- test/test_del_address.py
+++ test/test_del_address.py
@@ -1,6 +1,4 @@
 __author__ = "Alex"
 
 def test_delete_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.address.delete_first_address()
-    app.session.logout()
