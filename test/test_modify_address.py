161792cdf38477c87227c513e7f8c7896e677f95
Александр <leg020@inbox.ru>
2019-05-07 18:01:22 -0700

    Задание 9: Обеспечить выполнение предусловий в тестах для удаления и модификации для групп и контактов

--- test/test_modify_address.py
+++ test/test_modify_address.py
@@ -2,7 +2,11 @@ __author__ = 'Alex'
 from model.address import Address
 
 def test_modify_address_firstname(app):
+    if app.address.count() == 0:
+        app.address.create(Address(lastname='test'))
     app.address.modify_first_address(Address(firstname="9999"))
 
 def test_modify_address_middlename(app):
+    if app.address.count() == 0:
+        app.address.create(Address(firstname='test'))
     app.address.modify_first_address(Address(lastname="9999"))
