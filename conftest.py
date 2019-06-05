eebd2b66bc2502b9cb1a12e1a0b5fcbf26dd2028
Александр <leg020@inbox.ru>
2019-06-05 00:27:20 -0700

    Тест сравнения БД

--- conftest.py
+++ conftest.py
@@ -32,10 +32,10 @@ def app(request):
 @pytest.fixture
 def db(request):
     db_config = load_config(request.config.getoption('--target'))["db"]
-    dbfixture = DbFixture(host=db_config['host'],
-                          name=db_config['name'],
-                          user=db_config['user'],
-                          password=db_config['password'])
+    dbfixture = DbFixture(host=db_config["host"],
+                          name=db_config["name"],
+                          user=db_config["user"],
+                          password=db_config["password"])
     def fin():
         dbfixture.destroy
     request.addfinalizer(fin)
