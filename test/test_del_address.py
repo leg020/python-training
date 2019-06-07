376d980246c4ad3a32de9baf29ca24fa451edc3c
Александр <leg020@inbox.ru>
2019-06-07 01:15:17 -0700

    Отключаемые проверки пользовательского интерфейса

--- test/test_del_address.py
+++ test/test_del_address.py
@@ -2,7 +2,7 @@ __author__ = "Alex"
 from model.address import Address
 import random
 
-def test_delete_some_addrress(app, db):
+def test_delete_some_addrress(app, db, check_ui):
     if app.address.count() == 0:
         app.address.create(Address(firstname='test'))
     old_address = db.get_address_list()
@@ -12,3 +12,5 @@ def test_delete_some_addrress(app, db):
     assert len(old_address) - 1 == len(new_address)
     old_address.remove(address)
     assert old_address == new_address
+    if check_ui:
+        assert sorted(new_address, key=Address.id_or_max) == sorted(app.address.get_address_list(), key=Address.id_or_max)
