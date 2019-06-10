6be867baabcd226a3b9bd57b3a94c5a2f90cb892
Александр <leg020@inbox.ru>
2019-06-09 21:59:45 -0700

    Чтение из БД для обновления груп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,19 +1,23 @@
 __author__ = 'Alex'
 from model.group import Group
-from random import randrange
+import random
 
-def test_modify_group_name(app):
+def test_modify_group_name(app, db, check_ui):
     if app.group.count() == 0:
         app.group.create(Group(header='test'))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
-    new_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
+    text = "New group56"
+    group = random.choice(old_groups)
+    index = old_groups.index(group)
+    info = Group(name=text)
+    app.group.modify_group_by_id(group.id, info)
+    new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
+    group.name = text
     old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 def test_modyfy_group_header(app):
     if app.group.count() == 0:
